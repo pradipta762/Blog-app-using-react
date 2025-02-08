8be3ae95a27060f51c79913768fcaa43fdf5a4f4
Pradipta Kumar Dash <pradiptadash762@gmail.com>
2025-02-09 00:11:07 +0530

    (Update) : Blog Filter section updated

--- src/Components/Blog/BlogFilter.jsx
+++ src/Components/Blog/BlogFilter.jsx
@@ -3,7 +3,7 @@ import filterItem from '../../utils/filter'
 
 function BlogFilter({selectedCatagory, setSelectedCatagory}) {
   return (
-    <div className='border-b-2 border-gray-200 py-2 mb-4 pb-3 flex space-x-10'>
+    <div className='border-b-2 border-gray-200 py-2 mb-4 pb-3 flex space-x-10 overflow-x-scroll'>
       {filterItem.map((category) => (
         <button 
           className={`text-md hover:bg-slate-100 px-3 py-1 rounded-md focus:bg-slate-100 focus:text-orange-500 ${selectedCatagory === "All" && category.name == "All" ? "bg-slate-100 text-orange-500" : "bg-white text-black"}`}
